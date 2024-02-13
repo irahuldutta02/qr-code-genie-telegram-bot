21c84838115d8c1d828fdcf78da0e75727453afd
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-02-14 01:33:59 +0530

    add clear chat history logic

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const { Telegraf } = require("telegraf");
-const dotenv = require("dotenv");
 const axios = require("axios");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+let count = 1;
+
 bot.start((ctx) =>
-  ctx.reply("Welcome to the qr code genie bot! Send me any message or link I will return you a qr code of that message or link or send me a qr code I will return you the data of that qr code.")
+  ctx.reply(
+    "Welcome to the qr code genie bot! Send me any message or link I will return you a qr code of that message or link or send me a qr code I will return you the data of that qr code."
+  )
 );
 
 bot.command("hello", (ctx) => ctx.reply("Hello! ❤️"));
 
+//to clear the chat history
 bot.command("clear", async (ctx) => {
-  ctx.reply("Clearing the history...");
+  ctx.reply("Cleared the chat history!");
   const chatId = ctx.message.chat.id;
   const messageId = ctx.message.message_id;
-  for (let i = 1; i < messageId; i++) {
+  for (let i = messageId; i > 0; i--) {
     try {
       await ctx.telegram.deleteMessage(chatId, i);
     } catch (error) {
+      break;
       console.error(`Error deleting message with ID ${i}:`, error.description);
     }
   }
@@ -35,7 +40,6 @@ bot.on("text", (ctx) => {
     url: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${ctx.message.text}`,
   });
 });
-
 
 // to convert qr code to data
 bot.on("photo", async (ctx) => {
