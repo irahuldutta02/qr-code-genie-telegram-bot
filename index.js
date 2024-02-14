539772a9f920c80dac18c40cc5d3a81fb45de460
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-02-14 14:03:18 +0530

    .

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 let count = 1;
 
+console.log(bot.options);
+
 bot.start((ctx) => {
   ctx.reply(
     "Welcome to the qr code genie bot! Send me any message or link I will return you a qr code of that message or link or send me a qr code I will return you the data of that qr code."
@@ -65,8 +67,8 @@ bot.on("photo", async (ctx) => {
   }
 });
 
-bot.launch(); // starting the bot
-
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+bot.launch(); // starting the bot
